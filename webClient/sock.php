854babfa0c858345e07f069dc64d12bfa0d338c1
bklynjones <bklynjones@mac.com>
2011-05-24 16:56:37 -0400

    Added Scroll Library and CSS3 animation

---
+++ webClient/sock.php
@@ -0,0 +1,24 @@
+<!DOCTYPE >
+<head>
+    <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Expires" CONTENT="-1">
+   
+ <!--below is PHP from the server side of things-->
+		<div id="imageDirContents"><?php include("imageDirectoryReader.php"); ?></div>
+  
+   </head>
+<title>Jquery Gesture Test</title>
+
+<link href="default3.css" rel="stylesheet" type="text/css" />
+<body id="bod"class=" a4 poszero">
+<div id="box" >
+<div id="leftBar" class ="a4 lbounce" ></div>
+<div id="rightBar" class = "a4 rbounce"></div>
+
+
+</div>
+
+    <script type="text/javascript" src="jquery-1.4.4.min.js"></script>		
+		<script src="script3.js" type="text/javascript"></script>	
+
+</body>
